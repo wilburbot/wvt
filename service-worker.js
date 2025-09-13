1d4f7ce101b34a3c0e76a415e31d990b6a769c3b
John Joshua <110202785+wilburbot@users.noreply.github.com>
2025-09-13 03:18:10 -0700

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('wolfpack-vb-cache').then(cache => cache.addAll(['./index.html'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
